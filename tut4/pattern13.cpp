5251d7e44da3f84ac2ddff48a8c39b82d6c57637
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-23 00:17:30 +0530

    more prgs

---
+++ tut4/pattern13.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+    char start='A';
+    while (row<=n)
+    {
+     int col=1;
+     while (col<=n)
+     {
+        cout<<start;
+        start++;
+        col++;
+     }
+     cout<<endl;
+     row++;
+    }
+    
+}
---
+++ tut4/pattern14.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int i=1;
+while (i<=n)
+{
+    int j=1;
+    while (j<=n)
+    {
+char start='A'+i+j-2;
+        cout<<start;
+        // start++;
+        j++;
+    }
+    cout<<endl;
+    i++;
+}
+
+
+}
