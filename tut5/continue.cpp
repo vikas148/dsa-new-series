7bab357446edc2f749bde5844ebdffeaebef91b5
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-24 14:02:27 +0530

    tut5

---
+++ tut5/continue.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n=5;
+for (int i = 0; i < n; i++)
+{
+    cout<<"hii"<<endl;
+    cout<<"hey"<<endl;
+    continue;
+    cout<<"reply to de de"<<endl;
+}
+
+
+}
