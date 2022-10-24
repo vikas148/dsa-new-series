e069419f9795fb49a125eb9f7beb6d6ecedb1fcc
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-24 14:06:31 +0530

    for loop

---
+++ tut5/forloopq2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int n=10;
+//  cin>>n;
+int a=0;
+int b=1;
+cout<<a<<" "<<b<<" ";
+for (int i=1;i<=n;i++)
+{
+    int nextnum= a+b;
+    cout<<nextnum<<" ";
+
+    a=b;
+    b=nextnum;
+
+}
+
+
+  
+
+}
