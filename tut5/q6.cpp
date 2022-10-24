20823581fd69246ed8d5c56584be366c1afa7501
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-24 14:11:09 +0530

    Add files via upload

---
+++ tut5/q6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int  i = 0; i <= 5; i++)
+    {
+        cout<<i<<" ";
+        // i++; //it will give error cause we are updating it twice so compiler will confused in exicuting
+    }
+    
+}
