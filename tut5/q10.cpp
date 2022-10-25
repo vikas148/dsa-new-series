e37fd96a50f3941f277facef12cb3919a489342b
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-25 17:58:17 +0530

    Add files via upload

---
+++ tut5/q10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; i < 5; j++)
+        {
+            if (i+j==10)
+            {
+             break;
+            }
+            cout<<i<<" "<<j<<endl;
+            
+        }
+        
+    }
+    
+    
+}
