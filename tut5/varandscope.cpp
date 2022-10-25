3bb75b8e7eff79c91a9c4eff6d817f1d8d34cd2b
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-25 17:58:51 +0530

    Add files via upload

---
+++ tut5/varandscope.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=3;
+    cout<<a<<endl;
+    if (true)
+    {
+        cout<<a<<endl;
+        int a=5;
+        cout<<a<<endl;
+    }
+    
+}
