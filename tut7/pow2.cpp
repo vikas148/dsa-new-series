d0c290857d8eab3c4eed0297ba0aa4f302455861
VIKAS BAIRWA <71213518+vikas148@users.noreply.github.com>
2022-10-26 17:15:45 +0530

    Add files via upload

---
+++ tut7/pow2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int main()
+{
+int n;
+cin>>n;
+
+for (int i=0;i<=30;i++)
+{
+    int ans=pow(2,i);
+    if (ans==n)
+    {
+      //  return true;
+        cout<<n <<" is =" << ans;
+    }
+    
+   
+}
+return false;
+
+}
